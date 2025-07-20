970787ad62291c6b68abec596ec7bccb3949d6d9
Oluwaseyi <seyistry@gmail.com>
2025-07-20 10:35:59 +0100

    improve styles

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,18 +5,20 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button
-        title="Test 1"
-        styles="font-md rounded-md cursor-pointer bg-red-200 px-4 py-2"
-      />
-      <Button
-        title="Test 2"
-        styles="font-md rounded-md cursor-pointer bg-red-400 px-4 py-2 mx-4"
-      />
-      <Button
-        title="Test 3"
-        styles="font-md rounded-md cursor-pointer bg-red-600 text-white px-4 py-2"
-      />
+      <div className="my-2 flex gap-4">
+        <Button
+          title="Test 1"
+          styles="font-md rounded-sm cursor-pointer bg-red-200 px-4 py-2"
+        />
+        <Button
+          title="Test 2"
+          styles="font-lg rounded-lg cursor-pointer bg-red-400 px-4 py-2"
+        />
+        <Button
+          title="Test 3"
+          styles="font-xl rounded-full cursor-pointer bg-red-600 text-white px-4 py-2"
+        />
+      </div>
     </div>
   );
 };
