1cbb924c856b5a5163bb01bfcf6a2c9b45bf79bc
Oluwaseyi <seyistry@gmail.com>
2025-07-20 10:12:21 +0100

    add task 3

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const Pill = () => {
+const Pill: React.FC = (title: string) => {
   return (
-	<div>Pill</div>
-  )
-}
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
+  );
+};
 
-export default Pill
+export default Pill;
